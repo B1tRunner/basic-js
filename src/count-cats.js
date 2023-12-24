700a37b2d352750e292abae4f01f3679d0a1db7e
DmitryVV <dmitryvadiutin@gmail.com>
2023-12-24 13:50:03 +0300

    feat: implement cat ears func

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let counter = 0;
+  for (let row = 0; row < matrix.length; row += 1){
+    for (let item = 0; item < matrix[row].length; item += 1){
+      if (matrix[row][item] === '^^'){
+        counter += 1;
+      }
+    }
+  }
+  return counter;
+  /* throw new NotImplementedError('Not implemented'); */
   // remove line with error and write your code here
 }
 
